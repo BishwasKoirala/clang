b76748404f729d69a641b29e797990bbea289e9a
BishwasKoirala <wasubisu69@gmail.com>
2024-01-04 17:12:26 +0900

    dowhile を次にやります。

--- main.c
+++ main.c
@@ -1,27 +1,6 @@
 #include <stdio.h>
 
 int main (void) {
-int num;
-  scanf("%d",&num);
-  switch (num) {
-    case 1 :
-      printf("Harry");
-      break;
-    case 2 :
-      printf("ron");
-      break;
-    case 3 :
-      printf("hermiony");
-        break;
-    case 4 :
-      printf("milfy");
-        break;
-
-    // else statement
-    default:
-      printf("input correct num from 1 to 4");
-      break;
-  };
-
-  return 0 ;
+  // SO WHILE をやれ
+  return 0;
 };
